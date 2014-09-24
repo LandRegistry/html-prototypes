56a90b36052f6cc211202a4fb196c59246b4e591
Andy Porter <andyporter@Andys-MacBook-Pro.local>
2014-09-24 11:41:26 +0100

    first client - tweaked - no gender

--- app/server.py
+++ app/server.py
@@ -251,7 +251,7 @@ def conveyancer_add_clients_2_1():
 # Step 6 - add 1st client
 @app.route('/relationship-starts/conveyancer-add-client-1')
 def conveyancer_add_client_1_2_1():
-  return render_template('relationship-starts/conveyancer-add-client-1-2.1.html')
+  return render_template('relationship-starts/conveyancer-add-client-1-2.2.html')
 
 # Step 7 - add 2nd client
 @app.route('/relationship-starts/conveyancer-add-client-2')
