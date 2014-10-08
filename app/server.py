c2c1ae2176bbcbb2d74c1a99b16eae5ad5f7d180
Colm Britton <colmjude@gmail.com>
2014-10-08 09:46:42 +0100

    make flow data consistent

--- app/server.py
+++ app/server.py
@@ -393,7 +393,7 @@ def transfer_and_charge_citizen_1_sign_mortgage_3_0():
 # Step 3 - Client 1 signs transfer deed
 @app.route('/transfer-and-charge-v3/citizen-1-sign-transfer')
 def transfer_and_charge_citizen_1_sign_transfer_3_0():
-  json_data=open('app/static/data/ready-to-sign-transfer.json', "r")
+  json_data=open('app/static/data/transfer-signing-data.json', "r")
   data = json.load(json_data)
   return render_template('transfer/transfer-signing.html', next_page="/transfer-and-charge-v3/citizen-1-sms", data=data, role="citizen")
 
